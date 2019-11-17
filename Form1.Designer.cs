757100cf3e38c45f96ddf18fb20726451a8ae143
nfsiam <iamthenfs@gmail.com>
2019-11-18 05:10:37 +0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace CRUDPrac2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbRoll = new System.Windows.Forms.TextBox();
+            this.lblRoll = new System.Windows.Forms.Label();
+            this.tbFName = new System.Windows.Forms.TextBox();
+            this.lblFName = new System.Windows.Forms.Label();
+            this.tbAdd = new System.Windows.Forms.TextBox();
+            this.lblAdd = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblMob = new System.Windows.Forms.Label();
+            this.tbMob = new System.Windows.Forms.TextBox();
+            this.database1DataSet = new CRUDPrac2.Database1DataSet();
+            this.studentsTbBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.studentsTbTableAdapter = new CRUDPrac2.Database1DataSetTableAdapters.StudentsTbTableAdapter();
+            this.studentIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fatherNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rollNumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mobileDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentsTbBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.studentIDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.fatherNameDataGridViewTextBoxColumn,
+            this.rollNumberDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn,
+            this.mobileDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.studentsTbBindingSource;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 230);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(735, 208);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DataGridView1_CellMouseClick);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(32, 178);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(86, 33);
+            this.btnInsert.TabIndex = 5;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.BtnInsert_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(36, 39);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(44, 18);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name";
+            // 
+            // tbName
+            // 
+            this.tbName.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbName.Location = new System.Drawing.Point(130, 36);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(223, 25);
+            this.tbName.TabIndex = 0;
+            this.tbName.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // tbRoll
+            // 
+            this.tbRoll.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbRoll.Location = new System.Drawing.Point(130, 72);
+            this.tbRoll.Name = "tbRoll";
+            this.tbRoll.Size = new System.Drawing.Size(223, 25);
+            this.tbRoll.TabIndex = 2;
+            // 
+            // lblRoll
+            // 
+            this.lblRoll.AutoSize = true;
+            this.lblRoll.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRoll.Location = new System.Drawing.Point(36, 75);
+            this.lblRoll.Name = "lblRoll";
+            this.lblRoll.Size = new System.Drawing.Size(54, 18);
+            this.lblRoll.TabIndex = 4;
+            this.lblRoll.Text = "Roll No.";
+            // 
+            // tbFName
+            // 
+            this.tbFName.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbFName.Location = new System.Drawing.Point(543, 39);
+            this.tbFName.Name = "tbFName";
+            this.tbFName.Size = new System.Drawing.Size(223, 25);
+            this.tbFName.TabIndex = 1;
+            // 
+            // lblFName
+            // 
+            this.lblFName.AutoSize = true;
+            this.lblFName.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFName.Location = new System.Drawing.Point(471, 42);
+            this.lblFName.Name = "lblFName";
+            this.lblFName.Size = new System.Drawing.Size(57, 18);
+            this.lblFName.TabIndex = 8;
+            this.lblFName.Text = "F. Name";
+            // 
+            // tbAdd
+            // 
+            this.tbAdd.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbAdd.Location = new System.Drawing.Point(130, 109);
+            this.tbAdd.Name = "tbAdd";
+            this.tbAdd.Size = new System.Drawing.Size(223, 25);
+            this.tbAdd.TabIndex = 3;
+            // 
+            // lblAdd
+            // 
+            this.lblAdd.AutoSize = true;
+            this.lblAdd.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdd.Location = new System.Drawing.Point(36, 112);
+            this.lblAdd.Name = "lblAdd";
+            this.lblAdd.Size = new System.Drawing.Size(56, 18);
+            this.lblAdd.TabIndex = 6;
+            this.lblAdd.Text = "Address";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(252, 178);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(90, 33);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(472, 178);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(88, 33);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(673, 178);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(94, 33);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // lblMob
+            // 
+            this.lblMob.AutoSize = true;
+            this.lblMob.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMob.Location = new System.Drawing.Point(471, 75);
+            this.lblMob.Name = "lblMob";
+            this.lblMob.Size = new System.Drawing.Size(49, 18);
+            this.lblMob.TabIndex = 8;
+            this.lblMob.Text = "Mobile";
+            // 
+            // tbMob
+            // 
+            this.tbMob.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbMob.Location = new System.Drawing.Point(543, 72);
+            this.tbMob.Name = "tbMob";
+            this.tbMob.Size = new System.Drawing.Size(223, 25);
+            this.tbMob.TabIndex = 4;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // studentsTbBindingSource
+            // 
+            this.studentsTbBindingSource.DataMember = "StudentsTb";
+            this.studentsTbBindingSource.DataSource = this.database1DataSet;
+            // 
+            // studentsTbTableAdapter
+            // 
+            this.studentsTbTableAdapter.ClearBeforeFill = true;
+            // 
+            // studentIDDataGridViewTextBoxColumn
+            // 
+            this.studentIDDataGridViewTextBoxColumn.DataPropertyName = "StudentID";
+            this.studentIDDataGridViewTextBoxColumn.HeaderText = "StudentID";
+            this.studentIDDataGridViewTextBoxColumn.Name = "studentIDDataGridViewTextBoxColumn";
+            this.studentIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // fatherNameDataGridViewTextBoxColumn
+            // 
+            this.fatherNameDataGridViewTextBoxColumn.DataPropertyName = "FatherName";
+            this.fatherNameDataGridViewTextBoxColumn.HeaderText = "FatherName";
+            this.fatherNameDataGridViewTextBoxColumn.Name = "fatherNameDataGridViewTextBoxColumn";
+            // 
+            // rollNumberDataGridViewTextBoxColumn
+            // 
+            this.rollNumberDataGridViewTextBoxColumn.DataPropertyName = "RollNumber";
+            this.rollNumberDataGridViewTextBoxColumn.HeaderText = "RollNumber";
+            this.rollNumberDataGridViewTextBoxColumn.Name = "rollNumberDataGridViewTextBoxColumn";
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "Address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "Address";
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            // 
+            // mobileDataGridViewTextBoxColumn
+            // 
+            this.mobileDataGridViewTextBoxColumn.DataPropertyName = "Mobile";
+            this.mobileDataGridViewTextBoxColumn.HeaderText = "Mobile";
+            this.mobileDataGridViewTextBoxColumn.Name = "mobileDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 453);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.tbMob);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.tbFName);
+            this.Controls.Add(this.lblRoll);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.lblMob);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.tbRoll);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblFName);
+            this.Controls.Add(this.tbAdd);
+            this.Controls.Add(this.lblAdd);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentsTbBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbRoll;
+        private System.Windows.Forms.Label lblRoll;
+        private System.Windows.Forms.TextBox tbFName;
+        private System.Windows.Forms.Label lblFName;
+        private System.Windows.Forms.TextBox tbAdd;
+        private System.Windows.Forms.Label lblAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblMob;
+        private System.Windows.Forms.TextBox tbMob;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource studentsTbBindingSource;
+        private Database1DataSetTableAdapters.StudentsTbTableAdapter studentsTbTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn studentIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fatherNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rollNumberDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mobileDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CRUDPrac2
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = D:\Edu\Programs\C#\Practice\CRUDPrac2\Database1.mdf;Integrated Security=True");
+        int stdID;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadFromDB();
+        }
+
+        private void LoadFromDB()
+        {
+            
+            SqlCommand cmd = new SqlCommand("Select * from StudentsTb", con);
+            DataTable dt = new DataTable();
+
+            con.Open();
+
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            dt.Load(sdr);
+            con.Close();
+
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnInsert_Click(object sender, EventArgs e)
+        {
+            if(isValid())
+            {
+                SqlCommand cmd = new SqlCommand("Insert into studentsTb Values (@name, @FName, @Roll, @Address, @Mobile) ", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@name", tbName.Text);
+                cmd.Parameters.AddWithValue("@FName", tbFName.Text);
+                cmd.Parameters.AddWithValue("@Roll", tbRoll.Text);
+                cmd.Parameters.AddWithValue("@Address", tbAdd.Text);
+                cmd.Parameters.AddWithValue("@Mobile", tbMob.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Inserted successfully");
+
+                LoadFromDB();
+
+                ClearTBs();
+
+            }
+        }
+
+        private bool isValid()
+        {
+            if(tbName.Text== string.Empty)
+            {
+                MessageBox.Show("Name is empty");
+                return false;
+            }
+            else  if (tbFName.Text == string.Empty)
+            {
+                MessageBox.Show("Father's Name is empty");
+                return false;
+            }
+            else if (tbAdd.Text == string.Empty)
+            {
+                MessageBox.Show("Address is empty");
+                return false;
+            }
+            else if (tbMob.Text == string.Empty)
+            {
+                MessageBox.Show("Mobile is empty");
+                return false;
+            }
+            else if (tbRoll.Text == string.Empty)
+            {
+                MessageBox.Show("Roll number is empty");
+                return false;
+            }
+            else
+                return true;
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            ClearTBs();
+            stdID = 0;
+        }
+
+        private void ClearTBs()
+        {
+            tbAdd.Clear();
+            tbFName.Clear();
+            tbName.Clear();
+            tbMob.Clear();
+            tbRoll.Clear();
+        }
+
+        private void DataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            stdID = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells[0].Value.ToString());
+            tbName.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            tbFName.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            tbRoll.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            tbAdd.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+            tbMob.Text = dataGridView1.SelectedRows[0].Cells[5].Value.ToString();
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            if (stdID>0)
+            {
+                SqlCommand cmd = new SqlCommand("Update studentsTb set Name = @name, FatherName = @FName, RollNumber = @Roll, Address = @Address, Mobile = @Mobile where StudentId = @ID ", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@name", tbName.Text);
+                cmd.Parameters.AddWithValue("@FName", tbFName.Text);
+                cmd.Parameters.AddWithValue("@Roll", tbRoll.Text);
+                cmd.Parameters.AddWithValue("@Address", tbAdd.Text);
+                cmd.Parameters.AddWithValue("@Mobile", tbMob.Text);
+                cmd.Parameters.AddWithValue("@ID", stdID);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Updated successfully");
+
+                LoadFromDB(); 
+            }
+            else
+            {
+                MessageBox.Show("Select student row to update");
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (stdID > 0)
+            {
+                SqlCommand cmd = new SqlCommand("Delete from studentsTb where StudentId = @ID ", con);
+                cmd.CommandType = CommandType.Text;
+               
+                cmd.Parameters.AddWithValue("@ID", stdID);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Deleted successfully");
+
+                LoadFromDB();
+            }
+            else
+            {
+                MessageBox.Show("Select student row to delete");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.StudentsTb' table. You can move, or remove it, as needed.
+            this.studentsTbTableAdapter.Fill(this.database1DataSet.StudentsTb);
+
+        }
+    }
+}
